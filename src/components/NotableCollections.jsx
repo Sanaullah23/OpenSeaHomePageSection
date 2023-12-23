8f2a0f2764eb3197f6e7425963b83524feb8b7ee
Sanaullah23 <sanaullahb.019@gmail.com>
2023-12-24 01:38:47 +0500

    no changes

--- src/components/NotableCollections.jsx
+++ src/components/NotableCollections.jsx
@@ -11,7 +11,7 @@ const NotableCollections = () => {
                 <SingleNotableCollection/>
                 <SingleNotableCollection/>
                 <SingleNotableCollection/>
-                <SingleNotableCollection/>
+              
                 
                 
                 </div>
